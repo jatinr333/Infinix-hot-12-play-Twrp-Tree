31524677de73dd483efe5b5ac177c62d3f9dd528
jatinr333 <68556888+jatinr333@users.noreply.github.com>
2022-10-28 13:17:37 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X6816C-user
-add_lunch_combo omni_Infinix-X6816C-userdebug
-add_lunch_combo omni_Infinix-X6816C-eng
+add_lunch_combo twrp_Infinix-X6816C-user
+add_lunch_combo twrp_Infinix-X6816C-userdebug
+add_lunch_combo twrp_Infinix-X6816C-eng
