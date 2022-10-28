c4d241193b8ad4300845c49595c4667fede22067
jatinr333 <68556888+jatinr333@users.noreply.github.com>
2022-10-28 15:01:38 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_Infinix-X6816C.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_Infinix-X6816C-user \
-    twrp_Infinix-X6816C-userdebug \
-    twrp_Infinix-X6816C-eng
+    twrp_X6816C-user \
+    twrp_X6816C-userdebug \
+    twrp_-X6816C-eng
