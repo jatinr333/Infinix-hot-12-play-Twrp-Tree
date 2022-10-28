1922fac683b2df7c59d44db3bb6f9f63004da6f1
jatinr333 <68556888+jatinr333@users.noreply.github.com>
2022-10-28 14:57:47 +0530

    Update and rename twrp_Infinix-X6816C.mk to twrp_X6816C.mk

--- twrp_X6816C.mk
+++ twrp_X6816C.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from Infinix-X6816C device
-$(call inherit-product, device/infinix/Infinix-X6816C/device.mk)
+# Inherit from X6816C device
+$(call inherit-product, device/infinix/X6816C/device.mk)
 
-PRODUCT_DEVICE := Infinix-X6816C
-PRODUCT_NAME := twrp_Infinix-X6816C
+PRODUCT_DEVICE := X6816C
+PRODUCT_NAME := twrp_X6816C
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6816C
 PRODUCT_MANUFACTURER := infinix
@@ -26,4 +26,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="X6816C-ST683SBCDS-R-GL-220615V9"
 
-BUILD_FINGERPRINT := Infinix/X6816C-GL/Infinix-X6816C:11/RP1A.201005.001/220615V9:user/release-keys
+BUILD_FINGERPRINT := Infinix/X6816C-GL/X6816C:11/RP1A.201005.001/220615V9:user/release-keys
